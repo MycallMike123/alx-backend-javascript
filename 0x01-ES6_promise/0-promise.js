b794b67933e23a4fa67b2ce55fd18bff868f1bce
Michael Mwangi <michaelmichire2016@gmail.com>
2024-06-25 08:40:58 -0700

    Return a Promise using this prototype function getResponseFromAPI()

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,8 @@
+function getResponseFromAPI() {
+  return new Promise(() => {
+    // Our API call or asynchronous operation goes here
+    // Once the operation is completed, we resolve or reject the promise accordingly
+  });
+}
+
+export default getResponseFromAPI;
