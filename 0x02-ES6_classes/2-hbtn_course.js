109c86e71d16a2c34a8eb23db3ddafa7d6ed4cb6
Michael Mwangi <michaelmichire2016@gmail.com>
2024-06-27 02:49:09 -0700

    Implement a class named HolbertonCourse

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -4,6 +4,7 @@ export default class HolbertonCourse {
     this.length = length;
     this.students = students;
   }
+
   // setter for name
   set name(name) {
     if (typeof name !== 'string') {
@@ -11,22 +12,22 @@ export default class HolbertonCourse {
     }
     this._name = name;
   }
-  // getter for name
+
   get name() {
     return this._name;
   }
-  // setter for length
+
   set length(length) {
     if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
     }
     this._length = length;
   }
-  // getter for length
+
   get length() {
     return this._length;
   }
-  // setter for students
+
   set students(students) {
     if (
       !Array.isArray(students)
@@ -36,7 +37,7 @@ export default class HolbertonCourse {
     }
     this._students = students;
   }
-  // getter for students
+
   get students() {
     return this._students;
   }
