9dc505493b087e6cecf9805d929c94e147f862fc
Michael Mwangi <michaelmichire2016@gmail.com>
2024-08-20 02:10:12 -0700

    A function named displayMessage that prints in STDOUT the string argument

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,12 @@
+#!/usr/bin/env node
+
+/**
+ * Prints the given message to the standard output (STDOUT).
+ * @param {string} message - The message to be printed.
+ */
 function displayMessage(message) {
     console.log(message);
 }
 
+// Export the displayMessage function to be used in other modules
 module.exports = displayMessage;
