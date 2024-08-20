87206b48a2390566115c20b9893c85f53b3c7cdf
Michael Mwangi <michaelmichire2016@gmail.com>
2024-08-20 03:09:58 -0700

    A full server in a directory named full_server

---
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -0,0 +1,17 @@
+// setup server to use the defined routes
+
+const express = require('express');
+const router = require('./routes/index');
+
+const app = express();
+const port = 1245;
+
+app.use('/', router);
+app.use('/students', router);
+app.use('/students/:major', router);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+export default app;
