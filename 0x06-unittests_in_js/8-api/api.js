77325ade6a03a6429f296ea1bc0de5b923af15c9
Michael Mwangi <michaelmichire2016@gmail.com>
2024-08-22 09:00:38 -0700

    Basic Integration testing

---
+++ 0x06-unittests_in_js/8-api/api.js
@@ -0,0 +1,16 @@
+// create instance of express named app listening on port 7865
+// and log "API available on localhost port 7865" to browser console when server is started
+
+const express = require('express');
+const app = express();
+const port = 7865;
+
+app.get('/', (request, response) => {
+  response.send('Welcome to the payment system');
+});
+
+const server = app.listen(port, () => {
+  console.log(`API available on localhost port ${port}`);
+});
+
+module.exports = server; // Export the server instance
